cd9ffe242fa46d7aaaaca7288ab43f7ed64a4ff4
cbaredline <119635127+cbaredline@users.noreply.github.com>
2024-12-09 17:34:14 +0100

    Fixed CSS path for render

--- LeonitStublla.js
+++ LeonitStublla.js
@@ -3,14 +3,13 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve index.html on the home route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
