34249d19ea8fca3d4bcdc44293048e51e6543a9d
cbaredline <119635127+cbaredline@users.noreply.github.com>
2024-12-09 17:24:07 +0100

    Updated link to CSS

--- LeonitStublla.js
+++ LeonitStublla.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Serve static files from "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
 
-// Default route to serve index.html
+// Serve index.html on the home route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
