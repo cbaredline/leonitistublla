170ef0199367ed04f2123a24a0e172173ebff150
cbaredline <119635127+cbaredline@users.noreply.github.com>
2024-12-10 17:36:24 +0100

    Fix static file serving and MIME type issue

--- LeonitStublla.js
+++ LeonitStublla.js
@@ -4,15 +4,26 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files (like CSS, JS) from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    setHeaders: (res, filePath) => {
+        if (filePath.endsWith('.css')) {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
 
-// Serve the index.html file on the root route
-app.get('/', (req, res) => {
+// Serve index.html for all routes
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Basic error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
