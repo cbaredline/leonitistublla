f880635f8a4aafbd133446cf1a9cffce63649622
cbaredline <119635127+cbaredline@users.noreply.github.com>
2024-12-10 11:12:03 +0100

    Fixed static files path

--- LeonitStublla.js
+++ LeonitStublla.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+// Serve static files (like CSS, JS) from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Serve the index.html file on the root route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
